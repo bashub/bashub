cae5f14f6ee83971f3de26eff609f1335c847c02
tglman <tglman@tglman.com>
2014-08-08 23:15:59 +0100

    completed first implementation of server side command store

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,8 +9,26 @@ angular.module('bashubApp')
       })
     .controller('CommandCtrl', function ($scope,$http,$routeParams) {
     	$http.get('api/command/'+$routeParams.command).success(function (command){
-		$scope.name=command.name;
-		$scope.info=command.info;
-		$scope.commands=command.commands;
+		if(command.commands && command.name && command.info){
+			$scope.name=command.name;
+			$scope.info=command.info;
+			$scope.commands=command.commands;
+		} else {
+			
+			$scope.name='none';
+			$scope.info='none';
+			$scope.commands=[];
+		}
 	});
+	$scope.addCommand = function(){
+		$scope.commands.push({});
+	};
+	$scope.save = function() {
+		var command = {};
+		command.name = $scope.name;
+		command.info = $scope.info;
+		command.commands = $scope.commands;
+		$http.post('api/command/'+$routeParams.command,command).success( function () {
+		});
+	};
     });
